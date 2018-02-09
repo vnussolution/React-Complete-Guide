c860b0e3e86032329f5d5bb4241ee0add0a0749d
TRUC NGUYEN <vnussolution@gmail.com>
2018-02-09 11:15:34 -0800

    add radium for styling

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+import Radium,{StyleRoot}  from 'radium';
 
 
 class App extends Component {
   state ={
     persons:[
-      {name:'frankie',age:20},
-      {name:'tiffy',age:30},
-      {name:'truthy',age:50}
+      {id:'1',name:'frankie',age:20},
+      {id:'2',name:'tiffy',age:30},
+      {id:'3',name:'truthy',age:50}
     ],
     others:'123',
     showPersons:false
@@ -20,29 +21,44 @@ class App extends Component {
       this.setState({showPersons:!show});
   }
   
-  nameChangedHandler = (event)=>{
+  nameChangedHandler = (event,id)=>{
+    // const personIndex1 = this.state.persons.findIndex(id);
+    // console.log(' personIndex1 ',personIndex1);
+    
+    const personIndex = this.state.persons.findIndex(person=> person.id === id);
+    console.log(' personIndex ',personIndex);
+    
+    const newPerson = {...this.state.persons[personIndex]};
+    newPerson.name = event.target.value;
+    
+    const newPersons = [...this.state.persons];
+    // const newPersons = Object.assign({},this.state.persons);
+    newPersons[personIndex] = newPerson;
+    
     this.setState({
-      persons:[
-        {name:'newName',age:20},
-        {name:event.target.value,age:30},
-        {name:'truthy123',age:50}
-      ]
+      persons:newPersons
     });
   }
   
   deletePersonHandler = (personIndex)=>{
-    const persons = this.state.persons;
+    // const persons = this.state.persons;
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
     persons.splice(personIndex,1);
     this.setState({persons:persons});
   }
   
   render() {
     const style = {
-      backgroundColor:'white',
+      backgroundColor:'green',
+      text:'white',
       font:'inherit',
       border:'1px solid red',
       padding:'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'salmon',color:'black'
+      }
     }
     
     let persons = null;
@@ -51,25 +67,41 @@ class App extends Component {
       persons = (
           <div>
             {this.state.persons.map((person,index)=>{
-              return <Person name={person.name} age={person.age} click={()=> this.deletePersonHandler(index)} />
+              return <Person name={person.name} age={person.age} key={person.id}
+                          changed={(event)=>this.nameChangedHandler(event,person.id)}
+                          click={()=> this.deletePersonHandler(index)} />
             })}
             {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler}> Hobies : Pingpong ,tennis</Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
           </div>
       );
+      
+      style.backgroundColor = 'red';
+      style[':hover'] = {backgroundColor:'blue',color:'yellow'}
+    }
+    
+    const classes = [];
+    if(this.state.persons.length <=2) {
+      classes.push('red');
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold');
     }
     return (
-      <div className="App">
-      
-        <hr/>
-        <h1>Hi, Frankie here</h1>
-        <p> this is really working</p>
-        <button style={style} onClick={this.togglePersonHandler}>toggle me</button>
-        {persons}
-      </div>
+      <StyleRoot>
+          <div className="App">
+          
+            <hr/>
+            <h1>Hi, Frankie here</h1>
+            <p className={classes.join(' ')} > this is really working</p>
+            <button style={style} onClick={this.togglePersonHandler}>toggle me</button>
+            {persons}
+          </div>
+      </StyleRoot>
+          
     );
   }
 }
 
-export default App;
+export default Radium(App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Person from './Person.css';
+import Radium from 'radium';
+
 
 const person=(props)=>{
+    
+    const style = {'@media (max-width:900px)':{
+        width:'150px'
+    }};
     return (
-        <div className='Person'>
+        <div className='Person' style={style}>
             <p onClick={props.click}> I'm {props.name} and I am {props.age} years old</p>
             <h3> {props.children}</h3>
             <input type='text' 
@@ -14,4 +20,4 @@ const person=(props)=>{
     )
 };
 
-export default person;
+export default Radium(person);
